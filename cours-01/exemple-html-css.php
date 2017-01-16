d61866b91b27112047d34f935cf11b7e8a4339a6
Daniel Parpal <daniel.parpal@hotmail.com>
2017-01-16 18:05:44 -0500

    Code et assets du cours 1 (démonstrations)

---
+++ cours-01/exemple-html-css.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Bonjour bienvenue au cours</title>
+    <style type="text/css">
+        table {
+            border: 1px solid black;
+        }
+        tr {
+            background-color: blue;
+        }
+    </style>
+</head>
+<body>
+<table>
+    <tr>
+        <td>
+            asdf
+        </td>
+        <td>
+            asdfasdfasd
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <img src="expos.png"></img>
+        </td>
+        <td>
+            <a href="http://amazon.ca">Lien vers Amazon</a>
+        </td>
+    </tr>
+</table>
+</body>
+</html>
---
+++ cours-01/exemple-php.php
@@ -0,0 +1,26 @@
+<?php
+
+$produits = ['ABC', 'DEF'];
+
+// Cette variable contient la description du produit
+$nom = '<img src="expos.png"/>';
+
+echo $nom;
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+Ceci est direct
+<br>
+<br>
+<?php echo 'Je suis dans le body du html'; ?>
+
+
+</body>
+</html>
