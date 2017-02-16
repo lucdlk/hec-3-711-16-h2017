1148eaec9059e7f4925cd58fb23f8ee9314b367f
Daniel Parpal <daniel.parpal@hotmail.com>
2017-02-16 16:50:14 -0500

    Avec exercice 3.3 syntaxe des objets

---
+++ cours-06/exercice-3.3-syntaxe-des-objets.php
@@ -0,0 +1,24 @@
+<?php
+
+// mot-clé class
+// Nom de la classe
+// jeu d'accolades
+class CoursHec {
+
+    public $nomDuCours;
+
+    public $etudiantsInscrits = [];
+
+    public function inscrireEtudiant() {
+        // À venir
+    }
+
+    public function validerQteEtudiants() {
+        // À venir
+    }
+}
+
+$coursDAA = new CoursHec();
+var_dump($coursDAA);
+
+?>
