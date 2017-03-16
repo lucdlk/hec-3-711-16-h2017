c7a20ed6c34b5494e9f69f7740bec56dd13ef4fa
Daniel Parpal <daniel.parpal@hotmail.com>
2017-03-16 17:01:29 -0400

    Autres fichiers de démo

---
+++ cours-08/demo-css-balise-head.html.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <style type="text/css">
+        p {
+            font-size: 24px !important;
+            color: blue;
+        }
+    </style>
+</head>
+<body>
+<p>
+    asdfasldfkauslfhas
+</p>
+</body>
+</html>
---
+++ cours-08/demo-css-fichier-externe.html.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+<body>
+<p class="class-test">
+    asdfasldfkauslfhas
+</p>
+<div class="class-test">
+    asdfjashdflahjsdf
+</div>
+<div>
+    ruriuryiuryiuryiur
+</div>
+</body>
+</html>
