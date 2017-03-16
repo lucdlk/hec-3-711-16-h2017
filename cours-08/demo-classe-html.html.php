f79320808ca6fd43042c9c665c000d745e5ffcde
Daniel Parpal <daniel.parpal@hotmail.com>
2017-03-16 16:04:47 -0400

    Demos HTML

---
+++ cours-08/demo-classe-html.html.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+
+    <style type="text/css">
+        .impaire {
+            background-color: blue;
+        }
+
+        p {
+            width: 150px;
+        }
+    </style>
+</head>
+<body>
+<p class="impaire deuxieme">
+    asdf
+</p>
+<p class="paire">
+    eeee
+</p>
+<p class="impaire">
+    asdsaf
+</p>
+</body>
+</html>
---
+++ cours-08/demo-html.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<!--Ceci est commentaire-->
+<table>
+    <tr>
+        <td>Je suis dans la première cellule</td>
+        <td>Je suis dans la deuxième cellule</td>
+    </tr>
+</table>
+
+
+<img class="test" src="expos.png" alt="Photo des expos" height="100px" width="200px">
+<img class="test" src="">
+<div style="background-color: red;">
+    asdasdf
+</div>
+    <span style="background-color: blue;">
+        rerkjerkhrj
+    </span>
+
+
+</body>
+</html>
