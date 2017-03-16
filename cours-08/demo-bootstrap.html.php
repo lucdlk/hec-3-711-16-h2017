e251a95f8cd55b09064a4a64dd810ca7ff561064
Daniel Parpal <daniel.parpal@hotmail.com>
2017-03-16 17:40:10 -0400

    Démos avec bootstrap et css externe

---
+++ cours-08/demo-bootstrap.html.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+    <table class="table table-bordered table-striped">
+        <tr>
+            <td>asjdlfkajs</td>
+            <td>asjdlfkajs</td>
+            <td>asjdlfkajs</td>
+        </tr>
+        <tr>
+            <td>asjdlfkajs</td>
+            <td>asjdlfkajs</td>
+            <td>asjdlfkajs</td>
+        </tr>
+        <tr>
+            <td>asjdlfkajs</td>
+            <td>asjdlfkajs</td>
+            <td>asjdlfkajs</td>
+        </tr>
+    </table>
+    <div class="row">
+        <div class="col-md-6">
+            asdfasdfsdfasfrrrrr
+        </div>
+        <div class="col-md-6 hidden-sm hidden-xs">
+            rerasfarsf
+        </div>
+    </div>
+</div>
+</body>
+</html>
---
+++ cours-08/exercice-3.8.html.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="exercice-3.8.css" type="text/css" />
+</head>
+<body>
+<div class="alert alert-info">
+    Je suis à l'intérieur de la division
+</div>
+</body>
+</html>
