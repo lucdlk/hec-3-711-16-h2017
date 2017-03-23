57fe772b55329ea8e00fe2851f9fdfade9964298
Daniel Parpal <daniel.parpal@hotmail.com>
2017-03-23 15:56:28 -0400

    Demo d'un formulaire

---
+++ cours-09/demo-form.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<form>
+    <label for="name">Votre nom: </label>
+    <input type="text" name="name"/>
+    <input type="hidden" name=""/>
+    <input type="date" name="DOB"/>
+    <input type="password" name="password"/>
+    <input type="submit" value="Submit"/>
+</form>
+</body>
+</html>
